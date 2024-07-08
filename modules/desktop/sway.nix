f89068b3837dae4f7fdf2b0c0d056c8b0cb97716
lentilus <lentilus@mailbox.org>
2024-07-09 00:57:45 +0200

    add missing desktop packages and config

--- modules/desktop/sway.nix
+++ modules/desktop/sway.nix
@@ -8,12 +8,15 @@
         pkgs.kanshi
         pkgs.waybar
         pkgs.autotiling
-
+        pkgs.rofi
         pkgs.sway
+        pkgs.foot
     ];
 
     home.file = {
         "${config.xdg.configHome}/sway".source = "${dotfiles}/sway";
+        "${config.xdg.configHome}/waybar".source = "${dotfiles}/waybar";
+        "${config.xdg.configHome}/rofi".source = "${dotfiles}/rofi";
     };
 }
 
