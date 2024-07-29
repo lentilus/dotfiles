e29d74e265f5d3c632fac0f9e026f35abbeb2cbd
lentilus <lentilus@mailbox.org>
2024-07-29 22:49:39 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-nix --extra-experimental-features "flakes nix-command" run $PWD/#tmp-shell
-
-# install home-manager
-# nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
-# nix-channel --update
-# nix-shell '<home-manager>' -A install
-
-# nix-env -iA nixpkgs.home-manager
-
-
-# nixconf=$PWD/nixbuildconf/nix.conf
-# echo "experimental-features = nix-command flakes" > $nixconf
-# NIX_CONF_DIR=$nixconf; nix-shell -p home-manager 'home-manager switch --flake "$HOME"/dotfiles --impure'
+nix-shell -p home-manager --run 'home-manager switch $HOME/dotfiles'
