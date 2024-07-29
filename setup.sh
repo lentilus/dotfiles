6456972a057ad3a2287506682306c6b160f4889b
lentilus <lentilus@mailbox.org>
2024-07-29 02:42:38 +0200

    wip

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+nix --extra-experimental-features "flakes nix-command" run $PWD/#tmp-shell
 
 # install home-manager
 # nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
