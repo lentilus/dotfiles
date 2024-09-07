b95e94e8021da4147c851739553dc1c54ee2f11c
lentilus <lentilus@mailbox.org>
2024-09-07 18:56:13 +0200

    feat: remove redundant dotfiles now managed with home-manager

--- config/sway/scripts/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swayidle -w \
-	timeout 300 \
-	'swaylock -f -i /home/lentilus/Downloads/wallpaper.jpg' \
-	timeout 600 \
-	'swaymsg "output * dpms off"' resume 'swaymsg "output * dpms on"' \
-	before-sleep 'swaylock -f -i /home/lentilus/Downloads/wallpaper.jpg'
