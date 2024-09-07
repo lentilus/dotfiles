ff21ecb6187abb9d3821c6c5ded0248c73e23102
lentilus <lentilus@mailbox.org>
2024-09-07 18:50:15 +0200

    feat: pass

---
+++ home-manager/desktop/pass.nix
@@ -0,0 +1,20 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+    
+  options = {
+    pass.enable = lib.mkEnableOption "enable gnu pass";
+  };
+
+  config = lib.mkIf config.pass.enable {
+        programs.password-store = {
+            enable = true;
+            settings = {
+                PASSWORD_STORE_DIR = "${config.home.homeDirectory}/git/password-store";
+            };
+        };
+    };
+}
