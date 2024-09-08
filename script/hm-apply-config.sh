1ddcc857511d7e21a2eb98d0084c51a11ad5c4eb
lentilus <lentilus@mailbox.org>
2024-09-08 18:15:25 +0200

    feat: impure default build

--- script/hm-apply-config.sh
+++ script/hm-apply-config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Install home manager configuration
-nix build --extra-experimental-features "nix-command flakes" "../#homeConfigurations.default.activationPackage"
+# impure needed to read $HOME and $USER
+nix build --impure --extra-experimental-features "nix-command flakes" "../#homeConfigurations.default.activationPackage"
 ./result/activate
