fb297239f2f3259b04d0af0c24fae081b4d66dd1
lentilus <lentilus@mailbox.org>
2024-09-11 20:18:25 +0200

    testing: skhd config

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -8,4 +8,11 @@
 }: {
   services.nix-daemon.enable = true;
   nixpkgs.hostPlatform = "aarch64-darwin";
+
+  services.skhd = {
+    enable = true;
+    skhdConfig = ''
+        cmd + shift - return : open -n -a "kitty"
+    '';
+  };
 }
