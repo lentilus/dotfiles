d775ecb2aea8b51507b340c3fa2c2482bf0212fc
lentilus <lentilus@mailbox.org>
2024-09-11 19:39:28 +0200

    feat: make jump tmux declarative

--- modules/home-manager/core/tmux.nix
+++ modules/home-manager/core/tmux.nix
@@ -9,6 +9,7 @@
     tmux.enable = lib.mkEnableOption "foo";
   };
 
+
   config = lib.mkIf config.tmux.enable {
     programs.tmux = {
         enable = true; 
@@ -20,9 +21,36 @@
         plugins = [
             pkgs.tmuxPlugins.vim-tmux-navigator
         ];
-        extraConfig = ''
+        extraConfig = let
+            jump = pkgs.writeShellScriptBin "jump-tmux" ''
+                #!/bin/bash
+                # Heavily inspired by ThePrimagen's tmux sessionizer
+
+                target="$HOME/git"
+                selecthook='nvim -c "normal -"; $SHELL'
+
+                if [[ $# -eq 1 ]]; then
+                    selected=$1
+                else
+                    cd "$HOME/git" || exit
+                    selected=$(${pkgs.findutils}/bin/find $target -maxdepth 1 -type d -printf '%P\n' | fzf)
+                fi
+
+                if [[ -z $selected ]]; then
+                    exit 0
+                fi
+
+                selected_name=$(basename "$selected" | tr . _)
+
+                if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+                    tmux new-session -ds "$selected_name" -c "$selected" "$selecthook"
+                fi
+
+                tmux switch-client -t "$selected_name"
+            '';
+        in ''
             unbind C-f
-            bind-key -n C-f run-shell "tmux neww ~/.local/scripts/jump-tmux"
+            bind-key -n C-f run-shell "tmux neww ${jump}/bin/jump-tmux"
             bind -n M-H split-window -h -c "#{pane_current_path}"
             bind -n M-V split-window -v -c "#{pane_current_path}"
         '';
