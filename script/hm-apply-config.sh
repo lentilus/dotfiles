5009da21cf04d6fc71232e3c4a5898884c2010da
lentilus <lentilus@mailbox.org>
2024-09-13 01:29:09 +0200

    feat: move zshrc out of way for devpod

--- script/hm-apply-config.sh
+++ script/hm-apply-config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# remove the existing .zshrc, as it is in the way
+mv "$HOME/.zshrc" "$HOME/.zshrc.bak"
+
 # Install home manager configuration
 # impure needed to read $HOME and $USER
 nix build --impure --extra-experimental-features "nix-command flakes" "../#homeConfigurations.default.activationPackage"
