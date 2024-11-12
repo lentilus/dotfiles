d36c50d4803090bd83cf73fa243d44ce918558cd
lentilus <lentilus@mailbox.org>
2024-11-12 20:58:42 +0100

    formatting

--- config/nvim/lua/custom/plugins/treesitter.lua
+++ config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,43 +1,46 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        dependencies = {
-            {
-                'echasnovski/mini.ai',
-                version = '*',
-                config = function()
-                    require("mini.ai").setup()
-                end
-            },
-            {
-                'numToStr/Comment.nvim',
-                config = function()
-                    require("Comment").setup()
-                end
-            }
-        },
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ignore_install = {},
-                modules = {},
-                ensure_installed = {
-                    "c",
-                    "lua",
-                    "bash",
-                    "vim",
-                    "vimdoc",
-                    "query",
-                    "json"
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    disable = { "latex" },
-                    additional_vim_regex_highlighting = false,
-                },
-            })
-        end
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			{
+				"echasnovski/mini.ai",
+				version = "*",
+				config = function()
+					require("mini.ai").setup()
+				end,
+			},
+			{
+				"numToStr/Comment.nvim",
+				config = function()
+					require("Comment").setup()
+				end,
+			},
+			{
+				"nvim-treesitter/playground",
+			},
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ignore_install = {},
+				modules = {},
+				ensure_installed = {
+					-- "c",
+					-- "lua",
+					-- "bash",
+					-- "vim",
+					-- "vimdoc",
+					-- "query",
+					-- "json",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = { "latex" },
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
 }
