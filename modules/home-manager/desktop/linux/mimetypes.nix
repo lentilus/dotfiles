59c6bad2953ae3f21bfb4fd438e854e1173c6462
lentilus <lentilus@mailbox.org>
2025-01-05 14:27:41 +0000

    feat: set mimetypes

--- modules/home-manager/desktop/linux/mimetypes.nix
+++ modules/home-manager/desktop/linux/mimetypes.nix
@@ -12,12 +12,38 @@ in {
   config = lib.mkIf cfg.enable {
     xdg.mimeApps = {
       enable = true;
-      associations.added = {
-        "application/pdf" = ["zathura.desktop"];
+      defaultApplications = let
+        nvim = "nvim.desktop";
+        qutebrowser = "org.qutebrowser.qutebrowser.desktop";
+        zathura = "org.pwmt.zathura-pdf-mupdf.desktop";
+        mpv = "mpv.desktop";
+      in {
+        # nvim
+        "text/plain" = nvim;
+        "text/markdown" = nvim;
+        "application/json" = nvim;
+        "application/xml" = nvim;
+
+        # qutebrowser
+        "text/html" = qutebrowser;
+        "x-scheme-handler/http" = qutebrowser;
+        "x-scheme-handler/https" = qutebrowser;
+        "x-scheme-handler/about" = qutebrowser;
+        "x-scheme-handler/unknown" = qutebrowser;
+
+        "application/pdf" = zathura;
+
+        "image/png" = qutebrowser;
+        "image/jpg" = qutebrowser;
+        "image/jpeg" = qutebrowser;
+
+        # mpv
+        "video/mp4" = mpv;
+        "video/mpeg" = mpv;
+        "video/ogg" = mpv;
+        "video/x-matroska" = mpv;
       };
-      defaultApplications = {
-        "application/pdf" = ["zathura.desktop"];
-      };
+      associations = {};
     };
   };
 }
