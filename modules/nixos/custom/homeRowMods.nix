5ffc98b44866d8dabaa2bd2190b0b6408eb3779e
lentilus <lentilus@mailbox.org>
2025-01-30 16:42:59 +0000

    use tap-hold-release, adjust timings

--- modules/nixos/custom/homeRowMods.nix
+++ modules/nixos/custom/homeRowMods.nix
@@ -23,20 +23,20 @@ in {
               caps a s d f j k l ;
             )
             (defvar
-              tap-time 170
-              hold-time 200
+              tap-time 180
+              hold-time 190
             )
 
             (defalias
               capsesc esc
-              a (tap-hold $tap-time $hold-time a lsft)
-              s (tap-hold $tap-time $hold-time s lmet)
-              d (tap-hold $tap-time $hold-time d lalt)
-              f (tap-hold $tap-time $hold-time f lctl)
-              j (tap-hold $tap-time $hold-time j rctl)
-              k (tap-hold $tap-time $hold-time k ralt)
-              l (tap-hold $tap-time $hold-time l rmet)
-              ; (tap-hold $tap-time $hold-time ; rsft)
+              a (tap-hold-release $tap-time $hold-time a lsft)
+              s (tap-hold-release $tap-time $hold-time s lmet)
+              d (tap-hold-release $tap-time $hold-time d lalt)
+              f (tap-hold-release $tap-time $hold-time f lctl)
+              j (tap-hold-release $tap-time $hold-time j rctl)
+              k (tap-hold-release $tap-time $hold-time k ralt)
+              l (tap-hold-release $tap-time $hold-time l rmet)
+              ; (tap-hold-release $tap-time $hold-time ; rsft)
             )
 
             (deflayer base
