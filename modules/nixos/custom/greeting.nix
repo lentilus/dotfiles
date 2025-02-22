6e9b2f2132e18ccbd1be3f6b7371ed079f851fc3
lentilus <lentilus@mailbox.org>
2025-02-22 03:09:04 +0100

    wrap sway with zsh for hm env

--- modules/nixos/custom/greeting.nix
+++ modules/nixos/custom/greeting.nix
@@ -21,7 +21,8 @@ in {
       enable = true;
       settings = rec {
         initial_session = {
-          command = "${pkgs.sway}/bin/sway";
+          # we execute with zsh so the sway process has the hm env
+          command = "${pkgs.zsh}/bin/zsh -c ${pkgs.sway}/bin/sway";
           user = "lentilus";
         };
         default_session = initial_session;
