740aa5c929af2927fb75d9e5790395143138ef88
lentilus <lentilus@mailbox.org>
2025-02-24 14:32:11 +0100

    refactor: tidy sway config

--- features/home-manager/wayland-wm/sway.nix
+++ features/home-manager/wayland-wm/sway.nix
@@ -5,25 +5,16 @@
   ...
 }: {
   wayland.windowManager.sway = {
-    systemd = {
-      enable = true;
-    };
     enable = true;
+    systemd.enable = true;
     config = let
       mod = "Mod4";
-      onempty = ws: app: ''swaymsg "[workspace=${ws}]" focus || ${app}'';
-
-      # menus
       rofi = "${config.programs.rofi.package}/bin/rofi";
       rofi-pass = "${config.programs.rofi.pass.package}/bin/rofi-pass";
-
-      # media controls
-      volumeUp = "${pkgs.pamixer}/bin/pamixer -i 5";
-      volumeDown = "${pkgs.pamixer}/bin/pamixer -d 5";
-      volumeMute = "${pkgs.pamixer}/bin/pamixer -t";
+      pamixer = "${pkgs.pamixer}/bin/pamixer";
+      onempty = ws: app: ''swaymsg "[workspace=${ws}]" focus || ${app}'';
     in rec {
       modifier = mod;
-      defaultWorkspace = "workspace number 1";
       terminal = "kitty";
       menu = ''rofi -show drun'';
       window.titlebar = false;
@@ -40,20 +31,16 @@
         "${mod}+a" = null;
         "${mod}+Space" = null;
 
-        # workspace bindings
         "${mod}+1" = ''workspace 1; exec ${onempty "1" "${terminal} -- tmux a"}'';
         "${mod}+2" = ''workspace 2; exec ${onempty "2" "qutebrowser"}'';
-
         "${mod}+q" = "kill";
-        "${mod}+Shift+q" = "uwsm stop";
+
         "${mod}+w" = "exec ${rofi} -show window";
         "${mod}+x" = "exec ${rofi-pass}";
         "${mod}+g" = "exec ${pkgs.dlpdf}/bin/dlpdf";
-
-        # media keys
-        "XF86AudioLowerVolume" = "exec ${volumeDown}";
-        "XF86AudioRaiseVolume" = "exec ${volumeUp}";
-        "XF86AudioMute" = "exec ${volumeMute}";
+        "XF86AudioLowerVolume" = "exec ${pamixer} -i 5";
+        "XF86AudioRaiseVolume" = "exec ${pamixer} -d 5";
+        "XF86AudioMute" = "exec ${pamixer} -t";
       };
     };
   };
