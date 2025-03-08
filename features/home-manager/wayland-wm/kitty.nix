dd08b5bec813614ad9ce92f4ea84219b9888ac06
lentilus <lentilus@mailbox.org>
2025-03-08 18:21:46 +0100

    kitty: disable close confirmation prompt

--- features/home-manager/wayland-wm/kitty.nix
+++ features/home-manager/wayland-wm/kitty.nix
@@ -2,7 +2,10 @@
   home.sessionVariables.TERMINAL = lib.mkDefault "kitty";
   programs.kitty = {
     enable = true;
-    settings.enable_audio_bell = false;
+    settings = {
+      enable_audio_bell = false;
+      confirm_os_window_close = 0;
+    };
   };
   stylix.targets.kitty.variant256Colors = true;
 }
