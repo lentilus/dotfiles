3a0e7cbcef9ecb1383ff0d860db0167523930448
lentilus <lentilus@mailbox.org>
2025-03-08 18:22:13 +0100

    install libre-office, telegram

--- features/home-manager/wayland-wm/packages.nix
+++ features/home-manager/wayland-wm/packages.nix
@@ -3,8 +3,12 @@
     pkgs.poppler_utils
     pkgs.zathura
     pkgs.anki
+    pkgs.libreoffice
+
+    # communication
     pkgs.zapzap
     pkgs.signal-desktop
+    pkgs.telegram-desktop
   ];
   programs.mpv.enable = true;
 }
